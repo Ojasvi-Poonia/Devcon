340d01c19bf6bf96dc7a35c5103930cd8d925af5
Ojasvi-Poonia <i.ojasvipoonia@gmail.com>
2025-04-08 00:48:39 +0530

    Added CORS middleware in the program

--- backend/main.py
+++ backend/main.py
@@ -1,15 +1,23 @@
 from fastapi import FastAPI
 from auth import router as auth_router
 from user import router as user_router
+from fastapi.middleware.cors import CORSMiddleware
 
-app = FastAPI() #this will be used to define routes and handle requests
+app = FastAPI(title="devcon app") #this will be used to define routes and handle requests
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"], #for devleopment add frontend domain here\
+    allow_credentials = True,#allow cookies and credentials like JWT we are using here
+    allow_methods=["*"],#allow GET,PUT,POST methods we can also restrict as allow_methods=["GET","POST"]
+    allow_headers=["*"],
+)
 app.include_router(auth_router,prefix="/auth",tags=["Auth"])
 app.include_router(user_router,prefix="/user",tags=["User"])
 
 @app.get("/")
 def home():
-    return {"message": "This is some backend"} #this is a JASON response
+    return {"message": "welcome to devcon app", "status": "running"} #this is a JASON response
 #this is a route decorator which tells FAstapi to execute this function when a get request is made on the / page
-# if __name__ =="__main__":
-#     import uvicorn #uvicorn is ASGI Async. Server Gateway Interface server that runs fastapi applications
-#     uvicorn.run(app,host="127.0.0.1",port=8000,reload=True)
+if __name__ =="__main__":
+    import uvicorn #uvicorn is ASGI Async. Server Gateway Interface server that runs fastapi applications
+    uvicorn.run("main:app",host="0.0.0.0",port=8000,reload=True)
